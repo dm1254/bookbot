fa1ebb12ea8574845a8712b2f40f2687e4502e58
dm1254 <d.moreno1272@gmail.com>
2024-10-04 22:14:49 -0700

    C: Implemented new functions

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ def main():
 	get_path = "books/frankenstein.txt"
 	text = get_book(get_path)
 	print(text)
+	word_count = number_of_words(text)
+	print(f"Word Count:{word_count} \n")
+	Character_count = character_count(text)
+	print(Character_count)
 
 def get_book(path):
 	with open (path) as f:
 		return f.read()
 
-main()   
+def number_of_words(book):
+	return len(book.split())
+
+def character_count(Text):
+	lowered_char = Text.lower()
+	character_dict = {}
+	for i in range(0,len(lowered_char)):
+		character = lowered_char[i]
+		if character in character_dict:
+			character_dict[character] += 1
+		else:
+			character_dict[character] = 1
+	return character_dict
+main()  
